194eb8c191de45b8766e54c4260ac4f61cbbcfa7
Francisco das Chagas <francisco.chagas05@aluno.ifce.edu.br>
2023-11-02 16:42:33 -0300

    Initial commit

---
+++ src/app/calculadora/services/calculadora.service.ts
@@ -0,0 +1,54 @@
+/**
+ * Serviço responsável por executar as operações de uma 
+ * calculadora.
+ * 
+ * @author Francisco das Chagas B. da Silva<franciscobesilva@gmail.com>
+ * @since 1.0.0
+ */
+
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CalculadoraService {
+
+  //Constantes para identificar as operações da calculadora.
+  static readonly SOMA: string = '+';
+  static readonly SUBTRACAO: string = '-';
+  static readonly DIVISAO: string = '/';
+  static readonly MULTIPLICACAO: string = '*';
+
+  constructor() { }
+  /**
+   * Método que calcula uma operação matemática dado dois números.
+   * Suporta as operações soma, subtração, divisão e multiplicação.
+   *
+   * @param num1 number
+   * @param num2 number
+   * @param operacao string Operação a ser executada
+   * @return number Resultado da operação
+   */
+  calcular(num1: number, num2: number, operacao: string): number {
+  	let resultado: number; // armazena o resultado da operação
+
+  	switch(operacao) {
+  	  case CalculadoraService.SOMA:
+  	    resultado = num1 + num2;
+  		break;
+  	  case CalculadoraService.SUBTRACAO:
+  	    resultado = num1 - num2;
+  		break;
+  	  case CalculadoraService.DIVISAO:
+  	    resultado = num1 / num2;
+  		break;
+  	  case CalculadoraService.MULTIPLICACAO:
+  	    resultado = num1 * num2;
+  		break;
+  	  default:
+  	    resultado = 0;
+  	}
+
+  	return resultado;
+  }
+}
